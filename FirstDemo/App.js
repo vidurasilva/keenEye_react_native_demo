38c0416234542a0cdbf4e287e8ba0898f78ff6e3
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-06 17:54:42 +0530

    select main screen

--- FirstDemo/App.js
+++ FirstDemo/App.js
@@ -1,23 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,Alert, View , SafeAreaView, Button} from 'react-native';
+import React from 'react';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+// import SecondaryScreen from './app/screens/SecondaryScreen';
 
 export default function App() {
-  let me=1;
-  console.log("Run App");
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello Vidura Apps!</Text>
-      <Button title='Click me!' onPress={()=>Alert.alert("Are You Sure !", "you'r going to logout.",[{title:"Yes", onPress:()=>{console.log('No')}}])}> </Button>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen/>;
+  // return <SecondaryScreen/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
