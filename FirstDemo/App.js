80795289cb5cd0c85cf6b030c54f50e7616d9831
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-07 11:35:11 +0530

    navigation rought set

--- FirstDemo/App.js
+++ FirstDemo/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-// import SecondaryScreen from './app/screens/SecondaryScreen';
+import SecondaryScreen from './app/screens/SecondaryScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
-  return <WelcomeScreen/>;
-  // return <SecondaryScreen/>;
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="Details" component={SecondaryScreen} />
+ </Stack.Navigator>
+    </NavigationContainer>
+  );  
 }
+
 
